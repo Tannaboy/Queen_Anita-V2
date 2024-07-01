65be62b7a82c13a07cc3272f6a742c741f02980e
Tannaboy <nfriendly87@gmail.com>
2024-07-01 14:04:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
   caption: process.env.CAPTION || "`©QUEEN_ANITA-V2`",
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "Tannaboy_V2",
+  botname: process.env.BOT_NAME || "Tnaboy_V2",
   ownername: process.env.OWNER_NAME || "Mr Maraire",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
